4c3f9e8f3b85f1bea3763b18a6348be6227e389a
Tokachung <st1403@live.com>
2022-08-27 19:57:47 +1200

    Setting up backend/frontend communication

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+const express = require('express')
+
+// instantiating an app. Creating an instance of express
+const app = express()
+const workshops = [{ name: "Botany Library", volunteers: ["Beatrice Everall"] }, { name: "Pakuranga Library", volunteers: [] }]
+var cors = require('cors')
+
+app.use(cors())
+
+
+app.get('/workshops', function (req, res) {
+  res.send(workshops)
+})
+
+
+app.listen(3001)
