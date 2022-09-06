7f5bbb294d99e615ce7d3340a4a9d1c8fa96fddc
Shawn Tang <st1403@live.com>
2022-09-07 00:12:04 +1200

    backend setup

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,29 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 // instantiating an app. Creating an instance of express
 const app = express()
-const workshops = [{ name: "Botany Library", volunteers: ["Beatrice Everall"] }, { name: "Pakuranga Library", volunteers: [] }]
+const workshops = [{ id: 1, name: "Botany Library", volunteers: ["Beatrice Everall"] }, { id: 2, name: "Pakuranga Library", volunteers: [] }]
 var cors = require('cors')
 
 app.use(cors())
+app.use(bodyParser.json())
 
 
 app.get('/workshops', function (req, res) {
   res.send(workshops)
 })
 
+app.post('/workshop/:id/register', function (req, res) {
+  const workshopId = req.params.id;
+  const newVolunteer = req.body.name;
+  for (let workshop of workshops) {
+    if (workshop.id == workshopId) {
+      workshop.volunteers.push(newVolunteer)
+    }
+  }
+
+  res.send();
+})
 
 app.listen(3001)
