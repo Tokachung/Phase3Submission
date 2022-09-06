38ff51bd0277b2f956b93676941fb06a74134f77
Shawn Tang <st1403@live.com>
2022-09-07 01:17:42 +1200

    set up oAuth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,51 @@ import React, { useState, useEffect } from "react"
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios"
+import LoginButton from "./Login";
+import LogoutButton from "./Logout";
 
+// create tiles for each workshops, with a register for workshop button
+// OAuth
 
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
 
-  useEffect(async () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
 
-    const workshops = await axios.get("http://localhost:3001/workshops")
-    console.log(workshops)
+  // useEffect(async () => {
 
-  }, [])
+  //   const workshops = await axios.get("https://workshop-signup.herokuapp.com/workshops")
+  //   console.log(workshops)
+
+  //   const id = 1; // change depending on what the user clicks
+  //   const name = "Jonathan" // user input
+  //   const request = await axios.post(`https://workshop-signup.herokuapp.com/workshop/${id}/register`, { "name": name })
+
+  // }, [])
 
   return (
     <div className="App">
+      {
+        !isAuthenticated && (<LoginButton />)
+      }
+      {
+        isAuthenticated && (<LogoutButton />)
+      }
 
+      {
+        isAuthenticated && (
+          <div>
+            <img src={user.picture} alt={user.name} />
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        )
+
+      }
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//https://auth0.com/docs/quickstart/spa/react/interactive
+
+import { Auth0Provider } from "@auth0/auth0-react";
+// Auth0Provider stores the authentication state of your users
+// and the state of the SDK — whether Auth0 is ready to use or not.
+// It also exposes helper methods to log in and log out your users,
+// which you can access using the useAuth0() hook.
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+  domain="dev-v9zaiss3.us.auth0.com"
+  clientId="1U8VFcTh0mQfWbyFo2rxfAHy6CxSKBpB"
+  redirectUri={window.location.origin}
+>
+  <App />
+</Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
