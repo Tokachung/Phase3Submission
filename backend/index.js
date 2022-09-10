00ceace63129ceebea8d90d11a29a9b91923445f
Shawn Tang <st1403@live.com>
2022-09-11 04:03:40 +1200

    stuff2

--- backend/index.js
+++ backend/index.js
@@ -5,15 +5,15 @@ require('dotenv').config()
 // instantiating an app. Creating an instance of express
 const app = express()
 const workshops = [
- { id: 1, name: "Botany Library", volunteers: ["Beatrice Everall"], date: "15/09/22" }
-,{ id: 2, name: "Long Bay College", volunteers: [], date: "16/09/22"}
-,{ id: 3, name: "Rosebank Primary School", volunteers: [], date: "17/09/22"}
-,{ id: 4, name: "Puekoware School", volunteers: [], date: "18/09/22"}
-,{ id: 5, name: "Pukekohe Intermediate School", volunteers: [], date: "19/09/22"}
-,{ id: 6, name: "St Joseph's Catholic School", volunteers: [], date: "20/09/22"}
-,{ id: 7, name: "Oteha Valley School", volunteers: [], date: "21/09/22"}
-,{ id: 8, name: "Bombay School", volunteers: [], date: "22/09/22"}
-,{ id: 9, name: "Botany Library", volunteers: [], date: "23/09/22"}]
+ { id: 1, name: "Botany Library", volunteers: ["Beatrice Everall", "Jennifer Lowe", "Tianren Shen" ], date: "15/09/22" }
+,{ id: 2, name: "Long Bay College", volunteers: ["Jennifer Lowe"], date: "16/09/22"}
+,{ id: 3, name: "Rosebank Primary School", volunteers: ["Tianren Shen", "Jennifer Lowe"], date: "17/09/22"}
+,{ id: 4, name: "Puekoware School", volunteers: ["Zhelin Wang"], date: "18/09/22"}
+,{ id: 5, name: "Pukekohe Intermediate School", volunteers: ["John Smith", "Jennifer Lowe", "Tianren Shen"], date: "19/09/22"}
+,{ id: 6, name: "St Joseph's Catholic School", volunteers: ["Kevin Shin"], date: "20/09/22"}
+,{ id: 7, name: "Oteha Valley School", volunteers: ["April Chen"], date: "21/09/22"}
+,{ id: 8, name: "Bombay School", volunteers: ["Calvin Feng", "Tianren Shen"], date: "22/09/22"}
+,{ id: 9, name: "Botany Library", volunteers: ["Carissa Liew"], date: "23/09/22"}]
 var cors = require('cors')
 
 app.use(cors())
