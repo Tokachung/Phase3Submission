472d848c365d55e9d7833452ec6e90de18f340f8
Shawn Tang <st1403@live.com>
2022-09-10 15:23:59 +1200

    changes to workshops again

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,13 @@ require('dotenv').config()
 const app = express()
 const workshops = [{ id: 1, name: "Botany Library", volunteers: ["Beatrice Everall"], date: "15/09/22" }, 
 ,{ id: 2, name: "Long Bay College", volunteers: [], date: "16/09/22"}
-,{ id: 2, name: "Rosebank Primary School", volunteers: [], date: "17/09/22"}
-,{ id: 2, name: "Puekoware School", volunteers: [], date: "18/09/22"}
-,{ id: 2, name: "Pukekohe Intermediate School", volunteers: [], date: "19/09/22"}
-,{ id: 2, name: "St Joseph's Catholic School", volunteers: [], date: "20/09/22"}
-,{ id: 2, name: "Oteha Valley School", volunteers: [], date: "21/09/22"}
-,{ id: 2, name: "Bombay School", volunteers: [], date: "22/09/22"}
-,{ id: 2, name: "Botany Library", volunteers: [], date: "23/09/22"}]
+,{ id: 3, name: "Rosebank Primary School", volunteers: [], date: "17/09/22"}
+,{ id: 4, name: "Puekoware School", volunteers: [], date: "18/09/22"}
+,{ id: 5, name: "Pukekohe Intermediate School", volunteers: [], date: "19/09/22"}
+,{ id: 6, name: "St Joseph's Catholic School", volunteers: [], date: "20/09/22"}
+,{ id: 7, name: "Oteha Valley School", volunteers: [], date: "21/09/22"}
+,{ id: 8, name: "Bombay School", volunteers: [], date: "22/09/22"}
+,{ id: 9, name: "Botany Library", volunteers: [], date: "23/09/22"}]
 var cors = require('cors')
 
 app.use(cors())
